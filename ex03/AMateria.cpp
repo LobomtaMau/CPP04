cbbc0b213fd2bfcdfd89e47fa83288332ca29cf9
lobomtamau <lobomtamau@gmail.com>
2024-05-28 18:15:29 +0100

    4indo

---
+++ ex03/AMateria.cpp
@@ -0,0 +1,13 @@
+#include "AMateria.hpp"
+
+AMateria::AMateria(std::string const &type) : _type(type) {}
+
+AMateria::~AMateria() {}
+
+std::string const & AMateria::getType() const {
+    return _type;
+}
+
+void AMateria::use(ICharacter& target) {
+    std::cout << "* uses " << _type << " on " << target.getName() << " *" << std::endl;
+}
